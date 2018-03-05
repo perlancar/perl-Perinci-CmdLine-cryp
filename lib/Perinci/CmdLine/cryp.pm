87ff4ab8de21a05f5eeb2078d47661d5a6f1a8a5
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-03-05 17:16:11 +0700

    Release v0.001

--- lib/Perinci/CmdLine/cryp.pm
+++ lib/Perinci/CmdLine/cryp.pm
@@ -11,11 +11,12 @@ use Log::ger;
 use parent 'Perinci::CmdLine::Lite';
 
 sub hook_config_file_section {
-    my ($r, $section_name, $section_content) = @_;
+    my ($self, $r, $section_name, $section_content) = @_;
 
-    if ($section_name =~ m!\Aexchange\s*/\s*(\S+)(?:\s*/\s*(.+)\z!) {
-        my $xchg = $1;
-        my $nick = $2 // 'default';
+    if ($section_name =~ m!\A(exchange|wallet)\s*/\s*(\S+)(?:\s*/\s*(.+))?\z!) {
+        my $entity = $1;
+        my $xchg = $2;
+        my $nick = $3 // 'default';
         $r->{appdata}{cryp}{exchanges}{$xchg}{$nick} //= {};
         for (keys %$section_content) {
             $r->{appdata}{cryp}{exchanges}{$xchg}{$nick}{$_} =
@@ -23,6 +24,8 @@ sub hook_config_file_section {
         }
         return [204];
     }
+
+    [200];
 }
 
 1;
